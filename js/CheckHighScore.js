22484e04a2ddf19eb54872f923b25ffb5520b9eb
thembats <34658879+thembats@users.noreply.github.com>
2020-06-15 12:02:03 -0500

    Update text

--- js/CheckHighScore.js
+++ js/CheckHighScore.js
@@ -3,6 +3,6 @@ function CheckHighScore() {
 	var row = table.insertRow(0);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
-	cell1.innerHTML = "FUCK JANNIES";
-	cell2.innerHTML = "AND FUCK WYPIPO";
+	cell1.innerHTML = "cell1";
+	cell2.innerHTML = "cell2";
 }
